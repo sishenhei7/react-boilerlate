c63fde2767a2ee90700ad23e4d29e196e9502ce5
sishenhei7 <982505136@qq.com>
2020-01-06 09:52:23 +0800

    refactor: 删掉一些html和css

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './assets/logo.svg';
 import './styles/App.scss';
 
 const App: React.FC = () => (
   <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
       <p>
         Edit<code>src/App.tsx</code>and save to reload.
       </p>
       <p>{process.env.REACT_APP_FOO}</p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-          Learn React
-      </a>
     </header>
   </div>
 );
